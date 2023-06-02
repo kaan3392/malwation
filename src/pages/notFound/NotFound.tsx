2c317c33b2ba0dbf0aeb4ce178a5d2ffd5ca7dc0
kaan3392 <kaanalacali@gmail.com>
2023-06-02 20:04:58 +0300

    notFound page

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -3,7 +3,7 @@ import * as S from "./styled";
 const NotFound = () => {
   return (
     <S.Container>
-      <S.Not>Not Page Found</S.Not>
+      <S.Not>Page Not Found!</S.Not>
       <S.Number>404</S.Number>
       <S.GoBack to={"/"} >Go to Home Page</S.GoBack>
     </S.Container>
